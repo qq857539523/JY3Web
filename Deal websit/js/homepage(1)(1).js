178fd72241e1558ef491320524b1d5951646db5f
sxm <857539523@qq.com>
2019-03-31 23:38:15 +0800

    持续更新

---
+++ Deal websit/js/homepage(1)(1).js
@@ -0,0 +1,24 @@
+//主站
+$(function(){
+	$(".class1111").append("<img width='280px' height='150px' src='img/logo.svg' />");
+	$(".class1112").append("<img class='11121' width='70px' height='70px' src='img/横竖切换（竖）.svg' />");
+	$(".class1112").append("<span class='11122'>业务分类</span>")
+//	$("#div121").append();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	$("#div11121").css("display","none");
+	$(".class1112").click(function(){
+		$("#div11121").toggle();
+	})
+	
+})
